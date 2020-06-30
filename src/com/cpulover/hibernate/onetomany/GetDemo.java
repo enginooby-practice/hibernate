86f27e7a39ded5fca9a821781d617168e86aa1bc
cpulover <justacpulover@gmail.com>
2020-06-30 23:23:27 +0200

    Test retrieve object along with its one-to-many relationship association objects

---
+++ src/com/cpulover/hibernate/onetomany/GetDemo.java
@@ -0,0 +1,49 @@
+package com.cpulover.hibernate.onetomany;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.cpulover.hibernate.entity.Course;
+import com.cpulover.hibernate.entity.Instructor;
+import com.cpulover.hibernate.entity.InstructorDetail;
+
+public class GetDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			//get object
+			int id = 5;
+			Instructor tempInstructor=session.get(Instructor.class, id);
+			System.out.println("Get intrustor: "+tempInstructor);
+			
+			//get associated object by one-to-many relationship
+			System.out.println("List of associated courses: ");
+			System.out.println(tempInstructor.getCourses());
+
+			// commit transaction
+			session.getTransaction().commit();
+		} 
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			//handle connection leak issue
+			session.close();
+			factory.close();
+		}
+	}
+
+}
