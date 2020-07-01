6ad990e054454f2b8c1dc27660776a1f7e1f1769
cpulover <justacpulover@gmail.com>
2020-07-01 16:31:00 +0200

    Establish uni-directional one-to-many relationship with Review

--- src/com/cpulover/hibernate/entity/Course.java
+++ src/com/cpulover/hibernate/entity/Course.java
@@ -1,13 +1,18 @@
 package com.cpulover.hibernate.entity;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
 @Entity
@@ -15,14 +20,19 @@ import javax.persistence.Table;
 public class Course {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "id")
 	private int id;
-	
+
 	@Column(name = "title")
 	private String title;
-	
+
 	@ManyToOne(cascade = { CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH })
-	@JoinColumn(name="instructor_id")
+	@JoinColumn(name = "instructor_id")
 	private Instructor instructor;
+
+	@OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinColumn(name = "course_id")
+	private List<Review> reviews;
 
 	public Course() {
 
@@ -30,6 +40,14 @@ public class Course {
 
 	public Course(String title) {
 		this.title = title;
+	}
+
+	// convenience method for adding review
+	public void addReview(Review tempReview) {
+		if (reviews == null) {
+			reviews = new ArrayList<Review>();
+		}
+		reviews.add(tempReview);
 	}
 
 	public int getId() {
@@ -56,11 +74,17 @@ public class Course {
 		this.instructor = instructor;
 	}
 
+	public List<Review> getReviews() {
+		return reviews;
+	}
+
+	public void setReviews(List<Review> reviews) {
+		this.reviews = reviews;
+	}
+
 	@Override
 	public String toString() {
 		return "Course [id=" + id + ", title=" + title + "]";
 	}
-	
-	
 
 }
