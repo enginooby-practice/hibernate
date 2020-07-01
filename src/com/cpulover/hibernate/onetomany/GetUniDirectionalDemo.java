c55dff95de008bb43a0b234b17c479c0ad06acd6
cpulover <justacpulover@gmail.com>
2020-07-01 16:30:04 +0200

    Test create, get, delete in uni-directional one-to-many link

---
+++ src/com/cpulover/hibernate/onetomany/GetUniDirectionalDemo.java
@@ -0,0 +1,48 @@
+package com.cpulover.hibernate.onetomany;
+
+import java.io.Serializable;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.cpulover.hibernate.entity.Course;
+import com.cpulover.hibernate.entity.Instructor;
+import com.cpulover.hibernate.entity.InstructorDetail;
+import com.cpulover.hibernate.entity.Review;
+
+public class GetUniDirectionalDemo {
+
+	public static void main(String[] args) {
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Review.class).addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			// start a transaction
+			session.beginTransaction();
+
+			// get object
+			int id = 10;
+			Course tempCourse = session.get(Course.class, id);
+			System.out.println("Get course: " + tempCourse);
+
+			// get associated revies
+			System.out.println("List of associated reviews: ");
+			System.out.println(tempCourse.getReviews());
+
+			// commit transaction
+			session.getTransaction().commit();
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			// handle connection leak issue
+			session.close();
+			factory.close();
+		}
+	}
+
+}
