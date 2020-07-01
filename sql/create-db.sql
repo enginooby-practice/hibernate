bff2eb037999a7e6f0949794d08ca731c2f11a37
cpulover <justacpulover@gmail.com>
2020-07-01 22:39:11 +0200

    Update create-db.sql

--- sql/create-db.sql
+++ sql/create-db.sql
@@ -51,4 +51,51 @@ CREATE TABLE `course` (
 ) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=latin1;
 
 
+DROP TABLE IF EXISTS `review`;
+
+CREATE TABLE `review` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `comment` varchar(256) DEFAULT NULL,
+  `course_id` int(11) DEFAULT NULL,
+
+  PRIMARY KEY (`id`),
+
+  KEY `FK_COURSE_ID_idx` (`course_id`),
+
+  CONSTRAINT `FK_COURSE` 
+  FOREIGN KEY (`course_id`) 
+  REFERENCES `course` (`id`) 
+
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `student`;
+
+CREATE TABLE `student` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(45) DEFAULT NULL,
+  `last_name` varchar(45) DEFAULT NULL,
+  `email` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `course_student`;
+
+CREATE TABLE `course_student` (
+  `course_id` int(11) NOT NULL,
+  `student_id` int(11) NOT NULL,
+  
+  PRIMARY KEY (`course_id`,`student_id`),
+  
+  KEY `FK_STUDENT_idx` (`student_id`),
+  
+  CONSTRAINT `FK_COURSE_05` FOREIGN KEY (`course_id`) 
+  REFERENCES `course` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION,
+  
+  CONSTRAINT `FK_STUDENT` FOREIGN KEY (`student_id`) 
+  REFERENCES `student` (`id`) 
+  ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
 SET FOREIGN_KEY_CHECKS = 1;
